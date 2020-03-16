53340e206ccd61f01b599dfcb223fa94b3fdb243
lasridouae <lasridouae1@gmail.com>
2020-03-16 20:07:46 +0100

    adding workshop numb 4

---
+++ index4.js
@@ -0,0 +1,10 @@
+
+function format(numbers) {
+    let arr = "(+212) 777-227772".split('(+XXX) XXX-XXXXXX');
+     //boucle avec Foreach   d'un tableau de String
+     arr.forEach(numbers => console.log(numbers));
+  }
+
+  const num = [2,1,2,7,7,7,2,2,7,7,2];
+
+  console.log(format(num));
